f3023236695cff441c658ef731a39d5f2c236e23
Karl Tayeb <karl.tayeb7@gmail.com>
2022-09-14 12:53:28 -0500

    script to download ukbb phenotypes

---
+++ data/ukbb/request_ukbb.py
@@ -0,0 +1,57 @@
+# simple script to download ukbb summary stats by phenotype code
+import re
+import sys
+import os
+
+phecode = sys.argv[1]
+print(phecode)
+
+
+file = open('ukbb_manifest.tsv', "r")
+
+keys = ['Phenotype Code',
+        'Phenotype Description', 
+        'UK Biobank Data Showcase Link',
+        'Sex',
+        'File',
+        'wget command',
+        'AWS File',
+        'Dropbox File',
+        'md5s']																	
+
+relevant_keys = [
+    'Phenotype Code',
+    'Phenotype Description',
+    'Sex',
+    'AWS File'
+]
+
+
+def proc_line(line):
+    line = {k: v for k, v in zip(keys, line.strip().split('\t'))}
+    res = {k: line[k] for k in relevant_keys}
+    return res
+
+lines = []
+idx = 1
+for line in file:
+     if re.search(sys.argv[1], line):
+         phe = proc_line(line)
+         file_name = phe['AWS File'].split('/')[-1]
+         phe['wget command'] = f'wget {phe["AWS File"]} -O {phe["Phenotype Code"]}/{file_name}'
+
+         lines.append(phe)
+         print(
+             f'{idx}) Phenotype Code: {phe["Phenotype Code"]}, Description: {phe["Phenotype Description"]}, Sex: {phe["Sex"]}',
+         )
+         idx += 1
+
+dowload_idx = input("Which do you want to download? (enter a number, e.g. 1): ")
+
+donwload_idx = int(dowload_idx) - 1
+phe = lines[donwload_idx]
+
+# download
+if not os.path.exists(phe['Phenotype Code']):
+    os.makedirs(phe['Phenotype Code'])
+os.system(phe['wget command'])
